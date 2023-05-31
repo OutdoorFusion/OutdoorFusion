99922102f83138d95ec66a2c33e137528e59d30e
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2023-05-31 21:49:01 +0200

    veel onnodige lines weggehaald

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ import os
 
 data = pd.read_csv("C://Users//3dvec//OneDrive - De Haagse Hogeschool//Sem4//OneDrive - De Haagse Hogeschool//DEDSProject//Dashboard//Sales.csv")
 # data = pd.read_csv("C://Users//Vincent//OneDrive - De Haagse Hogeschool//DEDSProject//Dashboard//Sales.csv")
-=======
-
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -103,7 +101,6 @@ def chart_quantity_by_category():
 
     return figure.to_json()
 
-
 @app.route('/chartOmzetByCategory')
 def chart_omzet_by_category():
     dfOmzet = pd.read_csv("C://Users//3dvec//OneDrive - De Haagse Hogeschool//Sem4//OneDrive - De Haagse Hogeschool//OutdoorFusionDashboard//OutdoorFusion//OmzetPerCategorie.csv")
@@ -117,20 +114,14 @@ def chart_omzet_by_category():
         y=category_omzet['Omzet']
     )
 
-    # Create bar graph layout
     chart_layout = go.Layout(
         title='Welke categorieën leveren de meeste omzet',
         xaxis={'title': 'Category'},
         yaxis={'title': 'Omzet'}
     )
-
-    # Create figure
     figure = go.Figure(data=[chart_data], layout=chart_layout)
 
     return figure.to_json()
-
-
-
 
 #northwindProductQuantity
 @app.route('/chartProductQuantity')
@@ -166,9 +157,6 @@ def chartProductQuantity():
         return graphJSON
     else:
         return jsonify(error='Invalid chart type')
-
-
-
 
 @app.route('/chartVerkoopPrestaties') 
 def chartVerkoopPrestaties():
@@ -211,12 +199,6 @@ def chart():
     else:
         return jsonify(error='Invalid chart type or field A')
     
-
-
-
-
-
-
 data = pd.read_csv("C://Users//3dvec//OneDrive - De Haagse Hogeschool//Sem4//OneDrive - De Haagse Hogeschool//OutdoorFusionDashboard//OutdoorFusion//combined_revenue_profit_product.csv") #Totale Winst over Alle Producten Per maand
 data['Date'] = pd.to_datetime(data['Date'])
 
@@ -249,12 +231,8 @@ def chartOriginal():
     else:
         return jsonify(error='Invalid chart type or field A')
 
-
-
-
 data = pd.read_csv("C://Users//3dvec//OneDrive - De Haagse Hogeschool//Sem4//OneDrive - De Haagse Hogeschool//OutdoorFusionDashboard//OutdoorFusion//combined_revenue_profit_product2.csv")
 data['Date'] = pd.to_datetime(data['Date'])
-
 
 @app.route('/chartProductProfit')
 def chartProductProfit():
@@ -305,63 +283,6 @@ def chartProductProfit():
     else:
         return jsonify(error='Invalid chart type or field A')
 
-
-#PREDICTED REVENUE lijkt iets te veel op profit
-# @app.route('/chartProductRevenue')
-# def chartProductRevenue():
-#     chart_type = request.args.get('type')
-#     field_a = request.args.get('field_a')
-
-#     fig = None
-#     if chart_type == 'pie':
-#         if field_a == 'index':
-#             df = data[field_a].value_counts().reset_index()
-#             fig = px.pie(df, values=field_a, names='index')
-#         else:
-#             df = data.groupby(field_a)['Revenue'].sum().reset_index()
-#             fig = px.pie(df, values='Revenue', names=field_a)
-#     elif chart_type == 'bar':
-#         if field_a == 'product':
-#             product_data = data.groupby(['Product', pd.Grouper(key='Date', freq='M')]).agg({'Revenue': 'mean'}).reset_index()
-
-#             # Prepare the training data for all products
-#             X_train_product = product_data[product_data['Date'] < '2016-08-01']['Date'].apply(lambda x: x.toordinal()).values.reshape(-1, 1)
-#             y_train_product = product_data[product_data['Date'] < '2016-08-01']['Revenue'].values
-
-#             # Train the linear regression model for all products
-#             model_product = LinearRegression()
-#             model_product.fit(X_train_product, y_train_product)
-
-#             # Generate predictions for all products
-#             prediction_dates = pd.date_range(start='2016-08-01', end='2025-12-31', freq='M')
-#             X_pred_product = np.array([date.toordinal() for date in prediction_dates]).reshape(-1, 1)
-#             predicted_revenue_product = model_product.predict(X_pred_product)
-
-#             # Create a dataframe to store the predicted revenues
-#             predicted_data = pd.DataFrame({'Date': prediction_dates, 'Revenue': predicted_revenue_product})
-
-#             # Update the bar chart with the predicted data
-#             fig = px.bar(predicted_data, x='Date', y='Revenue', title='Average Revenue per Month',
-#                          labels={'Date': 'Date', 'Revenue': 'Average Revenue'})
-#             fig.update_layout(barmode='stack')
-#         else:
-#             df = data.groupby(field_a)['Revenue'].sum().reset_index()
-#             fig = px.bar(df, x=field_a, y='Revenue')
-#     else:
-#         return jsonify(error='Invalid chart type')
-
-#     if fig:
-#         graphJSON = fig.to_json()
-#         return graphJSON
-#     else:
-#         return jsonify(error='Invalid chart type or field A')
-
-
-
-
-    
-
-
 @app.route('/top10Products')
 def top10Products():
     df = data.groupby('Product')['Revenue'].sum().reset_index()
@@ -384,8 +305,6 @@ def top10ProductCategories():
 
     graphJSON = fig.to_json()
     return graphJSON
-
-
 
 @app.route('/chartProductProfitLine') 
 def chartProductProfitLine():         
@@ -421,24 +340,17 @@ def chartProductProfitLine():
 
     return fig.to_json()
 
-
-
-
-
 @app.route('/tablesVerkoopPrestaties') #table van verkoop prestaties is anders dan die van dashboard
 def tablesVerkoopPrestaties():
     table_optionsVerkoopPrestatietables = data['Product'].unique().tolist()  # Get unique products from "Product" column
     
     return jsonify(tablesVerkoopPrestatie=table_optionsVerkoopPrestatietables)
 
-
 @app.route('/tables') #table van dashboard. Anders dan die van VerkoopPrestaties
 def tables():
     table_options = data.columns.tolist()  
     
     return jsonify(tables=table_options)
-
-
 
 @app.route('/adventureworks')
 def adventureworks():
@@ -464,9 +376,6 @@ def northwindprestaties():
 def bikestoreprestaties():
     return render_template('bikestoreprestaties.html')
            
-
-
-
 @app.route('/product_data')
 def product_data():
     selected_product = request.args.get('product')
@@ -482,14 +391,12 @@ def product_data():
     # 'Sub_Category': int(product_data['Sub_Category'].iloc[0]),
     'Sub_Category': product_data['Sub_Category'].iloc[0],
     'revenue': int(product_data['Revenue'].iloc[0]),
-
     # 'date': product_data['Date'].iloc[0].strftime('%Y-%m-%d'),  #TODO sloopt op dit moment de 'Productinzien' pagina
     # 'Product_Category': product_data['Product_Category'].iloc[0]
 }
     response_data = {
         'productInfo': product_info
     }
-
     return jsonify(response_data)
 
 if __name__ == '__main__':
